8dab95b169afd9bbe5295100e5de7e00d2e458fd
Csaba Kocsis <csabakocsis94@gmail.com>
2026-01-08 19:39:54 +0100

    VS Setup + Game Mode created

---
+++ Source/Sandbox/CustomSandboxGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CustomSandboxGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SANDBOX_API ACustomSandboxGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
