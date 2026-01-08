3fd9bd9442d8847f6b0c0baa7aaf3b0667f3a9c7
Csaba Kocsis <csabakocsis94@gmail.com>
2026-01-08 19:33:44 +0100

    Empty project

---
+++ Source/Sandbox/SandboxGameMode.h
@@ -0,0 +1,22 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SandboxGameMode.generated.h"
+
+/**
+ *  Simple GameMode for a first person game
+ */
+UCLASS(abstract)
+class ASandboxGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASandboxGameMode();
+};
+
+
+
